9f66afe09a6803ad9bea9a12552ad23f4c0bc294
Tristan <tristan.tran@seniormedia.fr>
2023-10-25 12:20:17 +0200

    Add front to application

---
+++ config/tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/*.css',
+    './app/views/**/*.{erb,haml,html,slim}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
